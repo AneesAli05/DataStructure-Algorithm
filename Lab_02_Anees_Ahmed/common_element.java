21f5d2c66b4ee8546ac0411489d4cc537a269212
Anees Ahmed <aneessoomro471@gmail.com>
2024-09-03 14:28:24 +0500

    common_element.java

--- Lab_02_Anees_Ahmed/common_element.java
+++ Lab_02_Anees_Ahmed/common_element.java
@@ -9,5 +9,20 @@ package dsa_lab02;
  * @author M Sultan
  */
 public class common_element {
+     public static void main(String[] args) {
+        
+        String[] words={"abc","bcd","aaaa","cbc"};
+        char x='e';
+         List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            String str = words[i];
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == x) {
+                    list.add(i);
+                    break;
+                }
+      }
+}
+    }
     
 }
